d2fde0e01673362d9f3ef9ce37f5cbe3cb77ba56
Shunichiro Aki <register.develop@nana4.net>
2025-01-05 11:46:14 +0900

    fix: bulder

--- build.js
+++ build.js
@@ -16,4 +16,5 @@ await build({
   banner: {
     js: 'import { createRequire } from "module"; import url from "url"; const require = createRequire(import.meta.url); const __filename = url.fileURLToPath(import.meta.url); const __dirname = url.fileURLToPath(new URL(".", import.meta.url));',
   },
+  loader: { ".node": "file" },
 });
