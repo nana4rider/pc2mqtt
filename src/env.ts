2efe1cbe04c44028b55acbb0ffca204aad16fee9
Shunichiro Aki <register.develop@nana4.net>
2025-01-13 13:06:19 +0900

    feat: version up

--- src/env.ts
+++ src/env.ts
@@ -9,8 +9,11 @@ const env = cleanEnv(process.env, {
   PORT: num({ default: 3000 }),
   MQTT_TASK_INTERVAL: num({ default: 100 }),
   ENTITY_QOS: num({ choices: [0, 1, 2], default: 1 }),
+  // オンライン状態を送信する間隔
   AVAILABILITY_INTERVAL: num({ default: 10000 }),
+  // ON/OFF切り替え後、状態の更新を止める時間
   STATE_CHANGE_PAUSE_DURATION: num({ default: 30000 }),
+  // 状態を確認する間隔
   CHECK_ALIVE_INTERVAL: num({ default: 5000 }),
 });
 
