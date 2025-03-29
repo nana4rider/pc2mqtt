ceb615815bb8f109164302ffdeef37c09e8c5258
nana4rider <register.develop@nana4.net>
2025-03-30 04:08:34 +0900

    feat: lintを改善

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Entity } from "@/entity";
+import type { Entity } from "@/entity";
 import logger from "@/logger";
 import { setupAvailability } from "@/manager/availabilityManager";
 import setupMqttDeviceManager from "@/manager/mqttDeviceManager";
