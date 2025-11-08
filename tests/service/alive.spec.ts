bf4fa09502336a37de99a6d607997e52d6e468ca
nana4rider <register.develop@nana4.net>
2025-11-09 06:31:27 +0900

    fix: type

--- tests/service/alive.spec.ts
+++ tests/service/alive.spec.ts
@@ -1,7 +1,7 @@
 import type { RemoteConfig } from "@/entity";
 import requestAlive from "@/service/alive";
-import type { PingResponse } from "ping";
 import ping from "ping";
+import type { PingResponse } from "ping/types/parser/base";
 import { setTimeout } from "timers/promises";
 
 vi.mock("ping", () => ({
